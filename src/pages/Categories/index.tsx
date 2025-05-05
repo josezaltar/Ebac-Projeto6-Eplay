2cedfabb8cd1545b87071c1cbe51bf1aa0ea99c6
zezaltar <josezaltar@gmail.com>
2025-05-05 17:36:09 -0300

    Construindo  Paginas

---
+++ src/pages/Categories/index.tsx
@@ -0,0 +1,103 @@
+import ProductsList from '../../components/ProductsList'
+import Game from '../../models/Game'
+
+import resident from '../../assets/images/resident.png'
+import diablo from '../../assets/images/diablo.png'
+import zelda from '../../assets/images/zelda.png'
+import starwars from '../../assets/images/star_wars.png'
+
+const promocoes: Game[] = [
+  {
+    id: 1,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', ' R$ 250,00'],
+    image: resident
+  },
+  {
+    id: 2,
+    category: 'RPG',
+    description:
+      'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
+    title: 'Diablo IV',
+    system: 'PS5',
+    infos: ['5%', ' R$ 290,00'],
+    image: diablo
+  },
+  {
+    id: 3,
+    category: 'Ação',
+    description:
+      'Star Wars Jedi: Survivor é um próximo jogo de ação e aventura desenvolvido pela Respawn...',
+    title: 'Star Wars Jedi: Survivor',
+    system: 'Xbox Series X',
+    infos: ['15%', ' R$ 219,00'],
+    image: starwars
+  },
+  {
+    id: 4,
+    category: 'Ação',
+    description:
+      'Uma aventura épica pela terra e pelos céus de Hyrule aguarda em The Legend of Zelda™...',
+    title: 'Zelda: Tears of the Kingdom',
+    system: 'Nintendo',
+    infos: ['20%', ' R$ 150,00'],
+    image: zelda
+  }
+]
+
+const embreve: Game[] = [
+  {
+    id: 5,
+    category: 'RPG',
+    description:
+      'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
+    title: 'Diablo IV',
+    system: 'PS5',
+    infos: ['17/05/2025'],
+    image: diablo
+  },
+  {
+    id: 6,
+    category: 'RPG',
+    description:
+      'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
+    title: 'Diablo IV',
+    system: 'PS5',
+    infos: ['17/05/2025'],
+    image: diablo
+  },
+  {
+    id: 7,
+    category: 'RPG',
+    description:
+      'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
+    title: 'Diablo IV',
+    system: 'PS5',
+    infos: ['17/05/2025'],
+    image: diablo
+  },
+  {
+    id: 8,
+    category: 'RPG',
+    description:
+      'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
+    title: 'Diablo IV',
+    system: 'PS5',
+    infos: ['17/05/2025'],
+    image: diablo
+  }
+]
+const Categories = () => (
+  <>
+    <ProductsList games={promocoes} title="RPG" background="gray" />
+    <ProductsList games={embreve} title="Ação" background="black" />
+    <ProductsList games={promocoes} title="Aventura" background="gray" />
+    <ProductsList games={embreve} title="FPS" background="black" />
+  </>
+)
+
+export default Categories
