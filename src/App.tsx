043413399a2e65ff8dcc74b9cf5db9a44af964f9
zezaltar <josezaltar@gmail.com>
2025-05-05 17:56:25 -0300

    Construindo o rodapé

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from 'react-router-dom'
 import Header from './components/Header'
 import { GlobalCss } from './styles'
 import Rotas from './routes'
+import Footer from './components/Footer'
 function App() {
   return (
     <BrowserRouter>
@@ -10,6 +11,7 @@ function App() {
         <Header />
       </div>
       <Rotas />
+      <Footer />
     </BrowserRouter>
   )
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,39 @@
+import { Container, SectionTitle, Links, Link, FooterSection } from './styles'
+
+const currentYear = new Date().getFullYear()
+
+const Footer = () => (
+  <Container>
+    <SectionTitle className="container">
+      <FooterSection>
+        <SectionTitle> Categorias</SectionTitle>
+        <Links>
+          <li>
+            <Link>RPG</Link>
+            <Link>Ação</Link>
+            <Link>Aventura</Link>
+            <Link>Esportes</Link>
+            <Link>Simulação</Link>
+            <Link>Estrategia</Link>
+            <Link>FPS</Link>
+          </li>
+        </Links>
+      </FooterSection>
+      <FooterSection>
+        <SectionTitle> Acesso rápido</SectionTitle>
+        <Links>
+          <li>
+            <Link>Novidades</Link>
+            <Link>Promoções</Link>
+            <Link>Em breve</Link>
+          </li>
+        </Links>
+      </FooterSection>
+      <div>
+        <p>{currentYear} - &copy; EPLAY - Todos os direitos reservados</p>
+      </div>
+    </SectionTitle>
+  </Container>
+)
+
+export default Footer
