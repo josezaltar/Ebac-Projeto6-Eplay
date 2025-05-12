cdb189610a00f495d6011e49e6011ece4329f223
zezaltar <josezaltar@gmail.com>
2025-05-12 09:51:29 -0300

    Componentizando - Parte II

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,17 +1,30 @@
 import { ButtonContainer, ButtonLink } from './styles'
 
-type Props = {
+export type Props = {
   type: 'button' | 'link'
   title: string
   to?: string
   onclick?: () => void
   children?: string
+  variant?: 'primary' | 'secondary'
 }
 
-const Button = ({ type, title, to, onclick, children }: Props) => {
+const Button = ({
+  type,
+  title,
+  to,
+  onclick,
+  children,
+  variant = 'primary'
+}: Props) => {
   if (type === 'button') {
     return (
-      <ButtonContainer type="button" title={title} onClick={onclick}>
+      <ButtonContainer
+        variant={variant}
+        type="button"
+        title={title}
+        onClick={onclick}
+      >
         {children}
       </ButtonContainer>
     )
---
+++ src/components/Section/index.tsx
@@ -0,0 +1,16 @@
+import { Container, Tittle } from './styles'
+export type Props = {
+  title: string
+  background?: 'black' | 'gray'
+  children: JSX.Element
+}
+const Section = ({ title, background, children }: Props) => (
+  <Container background={background}>
+    <div className="container">
+      <Tittle>{title}</Tittle>
+      {children}
+    </div>
+  </Container>
+)
+
+export default Section
